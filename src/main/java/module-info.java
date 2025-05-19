406e128dc1a2bc984a42e297e6791ce16eff0ff6
Emil Stein <hanste01@easv365.dk>
2025-05-19 10:31:54 +0200

    initial push

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module org.media.examsprojekt {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.media.examsprojekt to javafx.fxml;
+    exports org.media.examsprojekt;
+}
